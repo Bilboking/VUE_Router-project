5c5e120f329076ed24dc3f08e90246a501127b9b
Bilboking <brkbendreality@gmail.com>
2020-12-27 06:11:53 -0600

    12-27-20

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,7 +1,8 @@
 <template>
     <h1>Jobs</h1>
-    <div v-for="job in jobs" :key="job.id"></div>
+    <div v-for="job in jobs" :key="job.id">
         <h2>{{ job.title }}</h2>
+    </div>
 </template>
 
 <script>

a21ee59d52bc2d9f5f0b8cf4d61c55db660cc627
Bilboking <brkbendreality@gmail.com>
2020-12-27 10:05:43 -0600

    12-27-20

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import NotFound from '../views/NotFound.vue'
 import Jobs from '../views/jobs/Jobs.vue'
 import JobDetails from '../views/jobs/JobDetails.vue'
 
@@ -27,6 +28,15 @@ const routes = [
     component: JobDetails,
     props: true
 
+  },
+  {//? Redirect route example.
+    path: '/all-jobs',
+    redirect: '/jobs',
+  },
+  { //? catchAll for 404 page not found 
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 

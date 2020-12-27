6ffdbcf10c6b58c2cfcd1b97ae8425f90255e2a3
Bilboking <brkbendreality@gmail.com>
2020-12-27 04:51:50 -0600

    12-27-20

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi aut atque nam numquam? Qui temporibus sed in cupiditate repellendus necessitatibus dolorum porro nam aut tempore deleniti vel nisi, eligendi maxime velit consequuntur officiis debitis? Excepturi, magni neque! Nulla doloribus dolor dignissimos maxime, modi magnam est labore corrupti illum aliquid error ullam. Quam optio repudiandae hic! Ratione, pariatur cum qui aliquam exercitationem odio quis, iste quam voluptate eius, velit excepturi! Tempore quasi corporis ex labore! Possimus aperiam, quam ex nihil sequi debitis nesciunt atque similique mollitia explicabo. Odio similique quam, hic vitae inventore modi architecto nisi vel est dolore nobis provident.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi aut atque nam numquam? Qui temporibus sed in cupiditate repellendus necessitatibus dolorum porro nam aut tempore deleniti vel nisi, eligendi maxime velit consequuntur officiis debitis? Excepturi, magni neque! Nulla doloribus dolor dignissimos maxime, modi magnam est labore corrupti illum aliquid error ullam. Quam optio repudiandae hic! Ratione, pariatur cum qui aliquam exercitationem odio quis, iste quam voluptate eius, velit excepturi! Tempore quasi corporis ex labore! Possimus aperiam, quam ex nihil sequi debitis nesciunt atque similique mollitia explicabo. Odio similique quam, hic vitae inventore modi architecto nisi vel est dolore nobis provident.</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
---
+++ src/views/Jobs.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Jobs</h1>
+    <div v-for="job in jobs" :key="job.id"></div>
+        <h2>{{ job.title }}</h2>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            jobs: [
+                {title: 'Ninja UX Designer', id: 1, details: 'lorem'},
+                {title: 'Ninja web  Developer', id: 2, details: 'lorem'},
+                {title: 'Ninja Vue Developer', id: 3, details: 'lorem'}
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
